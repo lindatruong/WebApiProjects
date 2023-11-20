07ac034f50063290ad4791b1a1b81b31d1322dd4
Linda Truong <ngoc.truong@stud.uni-corvinus.hu>
2023-11-20 14:08:41 +0100

    Még egy smiley:)

---
+++ 10gyak/Controllers/JokesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _10gyak.Controllers
+{
+    public class JokesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
